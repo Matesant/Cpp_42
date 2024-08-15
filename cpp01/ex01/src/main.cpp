3268b65e505b8ddef50bc164ab3cf6f00232e0ba
mateus <mateussantos1528@hotmail.com>
2024-08-14 21:02:51 -0300

    cpp01 ex01

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -3,12 +3,19 @@
 int main(void)
 {
 	int i;
-	
-	std::cout << "Creating a new zombie on the heap with newZombie(\"heapZombie\")" << std::endl;
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
+	std::cout << INVERSE << CYAN << "Creating A Horde of ZOMBIES!!!!!!!!" << RESET << std::endl;
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
 	Zombie *zombie;
 	zombie = zombieHorde(10, "fabio");
+	std::cout << INVERSE << CYAN << "CALLING THE HORDE!!!" << RESET << std::endl;
 	for (i = 0; i < 10; i++)
 		zombie->announce();
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
+	std::cout << INVERSE << CYAN << "RICK GRIMES IS HERE TO SAVE THE DAY!!!" << RESET << std::endl;
 	delete[] zombie;
+	std::cout << BLINK << RED << "-----------------------------------------------" << RESET << std::endl;
 	return (0);
 }
