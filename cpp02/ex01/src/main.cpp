929f064bded82c06b6fefb29ff1ae1158667107f
mateus <mateussantos1528@hotmail.com>
2024-08-20 15:03:47 -0300

    cpp02 ex01

--- cpp02/ex01/src/main.cpp
+++ cpp02/ex01/src/main.cpp
@@ -7,14 +7,20 @@ int	main(void)
 	Fixed const b(10);
 	Fixed const c(42.42f);
 	Fixed const d(b);
-	a = Fixed(1234.4321f);
+	a = 1234.4321f;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << INVERSE << "Floating point to integer" << RESET << std::endl;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << INVERSE << "Integer to floating point" << RESET << std::endl;
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
 	return (0);
 }
