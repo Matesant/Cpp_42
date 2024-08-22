532df983e4b54711990e3ee83f30ca08cc75058c
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-08-22 17:10:54 -0300

    cpp01

--- cpp01/ex00/includes/Zombie.hpp
+++ cpp01/ex00/includes/Zombie.hpp
@@ -6,11 +6,12 @@
 # include <string>
 
 const std::string RED = "\033[31m";
-const std::string GREEN = "\033122m";
-const std::string YELLOW = "\033[33m";
 const std::string CYAN = "\033[36m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
 const std::string BOLD = "\033[1m";
 const std::string BLINK = "\033[5m";
+const std::string INVERSE = "\033[7m";
 const std::string RESET = "\033[0m";
 
 // Class declaration
