a385af57617a11c53dd681056412e98d53504ff0
mateus <mateussantos1528@hotmail.com>
2024-10-12 22:41:07 -0300

    cpp07 ex01

---
+++ cpp07/ex01/includes/Iter.hpp
@@ -0,0 +1,51 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# define ORANGE "\033[38;5;208m"
+# define PINK "\033[38;5;213m"
+# define PURPLE "\033[38;5;135m"
+# define GREY "\033[38;5;246m"
+# define BROWN "\033[38;5;130m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define CYAN "\033[1;36m"
+# define YELLOW "\033[1;33m"
+# define MAGENTA "\033[1;95m"
+# define BLUE "\033[1;34m"
+# define BOLD "\033[1m"
+# define RESET "\033[0m"
+# define INVERSE "\033[7m"
+
+# include <iostream>
+# include <string>
+# include <sstream>
+# include <cmath>
+
+template <typename T>
+void Iter(T* array, size_t length, void (*func)(T const &))
+{
+	for (size_t i = 0; i < length; i++)
+		func(array[i]);
+}
+
+template <typename T>
+std::string toString(const T& value)
+{
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
+}
+
+template <typename T>
+void printColor(const T& value, const std::string& color)
+{
+    std::cout << color << toString(value) << RESET << std::endl;
+}
+
+template <typename T>
+void printColorOrange(const T& value)
+{
+    std::cout << ORANGE << toString(value) << RESET << std::endl;
+}
+
+#endif
---
+++ cpp07/ex01/src/main.cpp
@@ -0,0 +1,127 @@
+#include "./Iter.hpp"
+
+void	newLine(void)
+{
+	std::cout << std::endl;
+}
+inline double round(double x)
+{
+    return (x < 0.0) ? std::ceil(x - 0.5) : std::floor(x + 0.5);
+}
+
+void randonRound(const float& f)
+{
+	::printColor(round(f), MAGENTA);
+}
+
+void ifCharacterisOdd(const char& c)
+{
+	if (c % 2 == 0)
+		::printColor("Even", CYAN);
+	else
+		::printColor("Odd", MAGENTA);
+}
+
+void ifNumberisPrime(const int& n)
+{
+	if (n < 2)
+	{
+		::printColor("Not Prime", RED);
+		return;
+	}
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			::printColor("Not Prime", RED);
+			return;
+		}
+	}
+	::printColor("Prime", GREEN);
+}
+
+#include <unistd.h> // Para usleep
+
+#include <iostream>
+#include <unistd.h> // Para usleep
+
+#include <iostream>
+#include <unistd.h> // Para usleep
+
+#include <iostream>
+#include <unistd.h> // Para usleep
+
+#include <iostream>
+#include <unistd.h> // Para usleep
+
+void thatIsAllFolks()
+{
+    const char* message = "That's all folks!";
+    const int delay = 100000;
+    const char* colors[] = {YELLOW, GREEN, CYAN, MAGENTA, BLUE, RED};
+    const int numColors = sizeof(colors) / sizeof(colors[0]);
+    for (int i = 0; message[i] != '\0'; ++i)
+    {
+        std::cout << colors[i % numColors] << message[i] << RESET;
+        std::cout.flush();
+        usleep(delay);
+    }
+    std::cout << std::endl;
+}
+
+void printTag(const std::string& str) 
+{
+    const int totalWidth = 60;
+    int strLength = str.length();
+    if (strLength >= totalWidth) {
+        std::cout << str << std::endl;
+        return;
+    }
+
+    int hyphensEachSide = (totalWidth - strLength) / 2;
+    int extraHyphen = (totalWidth - strLength) % 2; // Para lidar com strings de comprimento ímpar
+
+    std::string hyphens(hyphensEachSide, '-');
+    std::cout << YELLOW << INVERSE << hyphens << str << hyphens << std::string(extraHyphen, '-') << RESET << std::endl;
+}
+
+void	printHyphen(void)
+{
+	std::cout << YELLOW << INVERSE << "------------------------------------------------------------" << RESET << std::endl;
+}
+
+int	main(void)
+{
+	char cArray[] = {'a', 'b', 'c', 'd'};
+	int iArray[] = {1, 2, 3, 4, 5};
+	float fArray[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
+
+	printHyphen();
+	printTag("Int Array");
+	printHyphen();
+
+	newLine();
+	::Iter(iArray, 5, ::printColorOrange);
+	newLine();
+
+	printHyphen();
+	printTag("Testing ifNumberisPrime with Iterator");
+	printHyphen();
+
+	newLine();
+	::Iter(cArray, 5, ::ifCharacterisOdd);
+	newLine();
+
+	printHyphen();
+	printTag("Testing randonRound with Iterator");
+	printHyphen();
+
+	newLine();
+	::Iter(fArray, 5, ::randonRound);
+	newLine();
+
+	printHyphen();
+	thatIsAllFolks();
+
+	return (0);
+}
