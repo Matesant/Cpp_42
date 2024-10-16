879e15b4dd100673eb217f333bb2406ff1ebf54b
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-10-16 17:53:45 -0300

    cpp08

--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -125,17 +125,23 @@ void	newLine(void)
 	std::cout << std::endl;
 }
 
+void pause(int milliseconds)
+{
+    clock_t end_time = clock() + milliseconds * CLOCKS_PER_SEC / 1000;
+    while (clock() < end_time);
+}
+
 void thatIsAllFolks()
 {
     const char* message = "That's all folks!";
-    const int delay = 100000;
+    const int delay = 100;
     const char* colors[] = {YELLOW, GREEN, CYAN, MAGENTA, BLUE, RED};
     const int numColors = sizeof(colors) / sizeof(colors[0]);
     for (int i = 0; message[i] != '\0'; ++i)
     {
         std::cout << colors[i % numColors] << message[i] << RESET;
         std::cout.flush();
-        usleep(delay);
+        pause(delay);
     }
     std::cout << std::endl;
 }
