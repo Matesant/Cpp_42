c1a3d5ac40da62a1c54486e2c70355b1d97e402e
mateus <mateussantos1528@hotmail.com>
2024-10-20 15:00:23 -0300

    cpp09 ex02

--- cpp09/ex02/includes/PmergeMe.hpp
+++ cpp09/ex02/includes/PmergeMe.hpp
@@ -1,11 +1,11 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-# define ORANGE "\033[38;5;208m"
-# define PINK "\033[38;5;213m"
-# define PURPLE "\033[38;5;135m"
-# define GREY "\033[38;5;246m"
-# define BROWN "\033[38;5;130m"
+# define ORANGE "\033[1;38;5;208m"
+# define PINK "\033[1;38;5;213m"
+# define PURPLE "\033[1;38;5;135m"
+# define GREY "\033[1;38;5;246m"
+# define BROWN "\033[1;38;5;130m"
 # define RED "\033[1;31m"
 # define GREEN "\033[1;32m"
 # define CYAN "\033[1;36m"
@@ -15,11 +15,13 @@
 # define BOLD "\033[1m"
 # define RESET "\033[0m"
 # define INVERSE "\033[7m"
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
 
 # include <algorithm>
-# include <iostream>
 # include <cmath>
 # include <cstdio>
+# include <iostream>
 # include <sstream>
 # include <string>
 # include <deque>
@@ -31,21 +33,22 @@ template <typename T> void printColor(const T &value, const std::string &color);
 
 class PmergeMe
 {
-  private:
+	private:
 	PmergeMe(const PmergeMe &other);
 	PmergeMe &operator=(const PmergeMe &other);
 
 	std::list<int> _list;
 	std::deque<int> _deque;
-	
-	//methods
-	bool _validNumber(const std::string &expression);
-	void _calculateDeque(void);
-	void _calculateList(void);
 
-  public:
+	// methods
+	bool _validNumber(const std::string &expression);
+	void _divideIntoPairsList(void);
+
+	public:
 	PmergeMe(void);
 	~PmergeMe(void);
+	void mergeInsertDeque(std::deque<int> &mainSet);
+	void mergeInsertList(std::list<int> &list);
 
 	void parseGood(const std::string &expression);
 };
@@ -59,7 +62,7 @@ template <typename T> std::string toString(const T &value)
 
 template <typename T> void printColor(const T &value, const std::string &color)
 {
-	std::cout << color << toString(value) << RESET << std::endl;
+	std::cout << color << toString(value) << RESET << ' ';
 }
 
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -10,7 +10,7 @@ PmergeMe::~PmergeMe(void)
 
 PmergeMe::PmergeMe(const PmergeMe &other)
 {
-	*this = other;
+	(void)other;
 }
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &other)
@@ -23,6 +23,8 @@ void PmergeMe::parseGood(const std::string &expression)
 {
 	std::istringstream iss(expression);
 	std::string token;
+	std::deque<int> deque;
+	std::list<int> list;
 
 	while (iss >> token)
 	{
@@ -31,40 +33,153 @@ void PmergeMe::parseGood(const std::string &expression)
 			printColor("Invalid expression", RED);
 			return ;
 		}
-		_deque.push_back(std::atoi(token.c_str()));
-		_list.push_back(std::atoi(token.c_str()));
+		deque.push_back(std::atoi(token.c_str()));
+		list.push_back(std::atoi(token.c_str()));
 	}
-	_calculateDeque();
-	//_calculateList();
-}
-
-void PmergeMe::_calculateDeque(void)
-{
-	
-}
-
-void PmergeMe::_calculateList(void)
-{
-	_list.sort();
-	std::list<int>::iterator it = _list.begin();
-	while (it != _list.end())
+	//print expression before sorting
+	std::cout << ORANGE << "Before: ";
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
+	{
+		printColor(*it, ORANGE);
+	}
+	std::cout << std::endl;
+	mergeInsertDeque(deque);
+	mergeInsertList(list);
+	std::cout << GREY << "After: ";
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
 	{
 		printColor(*it, GREY);
-		++it;
 	}
+	std::cout << std::endl;
+
+	std::cout << YELLOW << "After: ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+	{
+		printColor(*it, YELLOW);
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::mergeInsertDeque(std::deque<int> &deque)
+{
+    size_t size = deque.size();
+    if (size <= 1)
+        return ;
+
+    std::deque<int> mainSet, pendingNumberSet;
+    for (size_t i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            mainSet.push_back(std::max(deque[i], deque[i + 1]));
+            pendingNumberSet.push_back(std::min(deque[i], deque[i + 1]));
+        }
+        else
+        {
+            mainSet.push_back(deque[i]);
+        }
+    }
+    mergeInsertDeque(mainSet);
+    
+    int firstLargerElement = mainSet[0];
+    int smallerElement = -1;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if (i + 1 < size)
+        {
+            if (deque[i] == firstLargerElement || deque[i + 1] == firstLargerElement)
+            {
+                smallerElement = std::min(deque[i], deque[i + 1]);
+                break;
+            }
+        }
+    }
+    deque.clear();
+    deque = mainSet;
+
+    if (smallerElement != -1 && std::find(deque.begin(), deque.end(), smallerElement) == deque.end())
+        deque.push_front(smallerElement);
+
+    for (size_t i = 0; i < pendingNumberSet.size(); i++)
+    {
+        size_t size2 = deque.size();
+        if (pendingNumberSet[i] != smallerElement || size2 == (size - 1))
+        {
+            std::deque<int>::iterator it = std::lower_bound(deque.begin(), deque.end(), pendingNumberSet[i]);
+            deque.insert(it, pendingNumberSet[i]);
+        }
+    }
+}
+
+void PmergeMe::mergeInsertList(std::list<int> &list)
+{
+    size_t size = list.size();
+    if (size <= 1)
+        return;
+
+    std::list<int> mainSet, pendingNumberSet;
+    std::list<int>::iterator it = list.begin();
+    for (size_t i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            int first = *it++;
+            int second = *it++;
+            mainSet.push_back(std::max(first, second));
+            pendingNumberSet.push_back(std::min(first, second));
+        }
+        else
+        {
+            mainSet.push_back(*it++);
+        }
+    }
+    mergeInsertList(mainSet);
+
+    int firstLargerElement = mainSet.front();
+    int smallerElement = -1;
+
+    it = list.begin();
+    for (size_t i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            int first = *it++;
+            int second = *it++;
+            if (first == firstLargerElement || second == firstLargerElement)
+            {
+                smallerElement = std::min(first, second);
+                break;
+            }
+        }
+    }
+    list.clear();
+    list = mainSet;
+
+    if (smallerElement != -1 && std::find(list.begin(), list.end(), smallerElement) == list.end())
+        list.push_front(smallerElement);
+
+    for (std::list<int>::iterator it = pendingNumberSet.begin(); it != pendingNumberSet.end(); ++it)
+    {
+        if (*it != smallerElement || list.size() == (size - 1))
+        {
+            std::list<int>::iterator pos = std::lower_bound(list.begin(), list.end(), *it);
+            list.insert(pos, *it);
+        }
+    }
 }
 
 bool PmergeMe::_validNumber(const std::string &expression)
 {
-	std::istringstream iss(expression);
-	int l;
-	iss >> std::noskipws >> l;
-	if (iss.eof() && !iss.fail())
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    std::istringstream iss(expression);
+    long long l;
+    iss >> std::noskipws >> l;
+    if (iss.eof() && !iss.fail() && l <= MAX_INT && l >= MIN_INT)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
